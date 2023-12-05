8f4efa799ff6fb23762dc1e884c14f4660825e22
musella95 <musellamatteoo@hotmail.com>
2023-12-05 16:00:40 +0100

    main done

--- src/org/lessons/java/geometria/Main.java
+++ src/org/lessons/java/geometria/Main.java
@@ -1,2 +1,17 @@
-package org.lessons.java.geometria;public class Main {
+package org.lessons.java.geometria;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Dammi la base");
+        int userBase = Integer.parseInt(scanner.nextLine());
+        System.out.println("Dammi l'altezza");
+        int userAltezza = Integer.parseInt(scanner.nextLine());
+        Rettangolo myRettangolo = new Rettangolo(userAltezza,userBase);
+        System.out.println("questo è il perimetro: " + myRettangolo.calcoloPerimetro());
+        System.out.println("questa è l'aria" + myRettangolo.calocoloAria());
+
+    }
 }
--- src/org/lessons/java/geometria/Rettangolo.java
+++ src/org/lessons/java/geometria/Rettangolo.java
@@ -1,2 +1,29 @@
-package org.lessons.java.geometria;public class Rettangolo {
-}
+package org.lessons.java.geometria;
+
+public class Rettangolo {
+
+        //ATTRIBUTI
+        int altezza;
+        int base;
+        //COSTRUTTORE
+
+        Rettangolo(int altezza, int base){
+            this.altezza=altezza;
+            this.base=base;
+        }
+        //METODO X CALCOLARE L'ARIA
+        int calocoloAria(){
+            return base * altezza;
+        }
+        //METODO X CALCOLARE IL PERIMETERO
+        int calcoloPerimetro(){
+            return (2*base)+(2*altezza);
+        }
+        //METODO X DISEGNARE RETTANGOLO IN CONSOLE
+    void disgena(){
+        for (int i = 0; i <base ; i++) {
+            System.out.println();
+        }
+    }
+    }
+
